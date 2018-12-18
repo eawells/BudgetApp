82070ff390e8168fb119399be239fd27dcca01f0
Leidy Russell <leidysemail@gmail.com>
2018-12-18 17:02:46 -0600

    subbudget class added and budget template modified

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subbudget } from '../subbudget';
 
 @Component({
   selector: 'app-budget',
@@ -8,10 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class BudgetComponent implements OnInit {
 
   totalLabel = 'Total Budget';
+  totalBudget = 400;
+
+  subbudget: Subbudget = {
+    name: 'Groceries',
+    amount: 200
+  }
   
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
---
+++ src/app/subbudget.ts
@@ -0,0 +1,4 @@
+export class Subbudget {
+    name: string;
+    amount: number;
+}
